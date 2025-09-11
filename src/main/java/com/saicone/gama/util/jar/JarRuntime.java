b133a017ee0885c0423ef560855d906db51dcfa7
Rubén <44579213+Rubenicos@users.noreply.github.com>
2025-09-10 18:00:21 -0600

    Add jar runtime javadoc

--- src/main/java/com/saicone/gama/util/jar/JarRuntime.java
+++ src/main/java/com/saicone/gama/util/jar/JarRuntime.java
@@ -30,21 +30,51 @@ import java.util.jar.JarFile;
  */
 public class JarRuntime extends LinkedHashMap<String, Class<?>> {
 
+    /**
+     * Create a JarRuntime with a class to read classes from its code source.
+     *
+     * @param clazz the class to read classes from its code source.
+     * @return      a newly generated JarRuntime.
+     * @throws IOException if an I/O error occurs.
+     */
     @NotNull
     public static JarRuntime valueOf(@NotNull Class<?> clazz) throws IOException {
         return valueOf(clazz.getClassLoader(), clazz);
     }
 
+    /**
+     * Create a JarRuntime with specified class loader and class to read classes from its code source.
+     *
+     * @param classLoader the class loader to get classes.
+     * @param clazz       the class to read classes from its code source.
+     * @return            a newly generated JarRuntime.
+     * @throws IOException if an I/O error occurs.
+     */
     @NotNull
     public static JarRuntime valueOf(@NotNull ClassLoader classLoader, @NotNull Class<?> clazz) throws IOException {
         return valueOf(classLoader, clazz.getProtectionDomain().getCodeSource().getLocation());
     }
 
+    /**
+     * Create a JarRuntime with the default class loader and url to read classes from it.
+     *
+     * @param url the url to read classes from it.
+     * @return    a newly generated JarRuntime.
+     * @throws IOException if an I/O error occurs.
+     */
     @NotNull
     public static JarRuntime valueOf(@NotNull URL url) throws IOException {
         return valueOf(JarRuntime.class.getClassLoader(), url);
     }
 
+    /**
+     * Create a JarRuntime with specified class loader and url to read classes from it.
+     *
+     * @param classLoader the class loader to get classes.
+     * @param url         the url to read classes from it.
+     * @return            a newly generated JarRuntime.
+     * @throws IOException if an I/O error occurs.
+     */
     @NotNull
     public static JarRuntime valueOf(@NotNull ClassLoader classLoader, @NotNull URL url) throws IOException {
         File file;
@@ -62,11 +92,24 @@ public class JarRuntime extends LinkedHashMap<String, Class<?>> {
         }
     }
 
+    /**
+     * Create a JarRuntime with the default class loader and jar file to read classes from it.
+     *
+     * @param file the jar file to read classes from it.
+     * @return     a newly generated JarRuntime.
+     */
     @NotNull
     public static JarRuntime valueOf(@NotNull JarFile file) {
         return valueOf(JarRuntime.class.getClassLoader(), file);
     }
 
+    /**
+     * Create a JarRuntime with specified class loader and jar file to read classes from it.
+     *
+     * @param classLoader the class loader to get classes.
+     * @param file        the jar file to read classes from it.
+     * @return            a newly generated JarRuntime.
+     */
     @NotNull
     public static JarRuntime valueOf(@NotNull ClassLoader classLoader, @NotNull JarFile file) {
         final JarRuntime jarRuntime = new JarRuntime(classLoader);
@@ -80,24 +123,50 @@ public class JarRuntime extends LinkedHashMap<String, Class<?>> {
 
     private final ClassLoader classLoader;
 
+    /**
+     * Create a new JarRuntime with the default class loader.
+     */
     public JarRuntime() {
         this(JarRuntime.class.getClassLoader());
     }
 
+    /**
+     * Create a new JarRuntime with the specified parameters.
+     *
+     * @param classLoader the class loader to get classes.
+     */
     public JarRuntime(@NotNull ClassLoader classLoader) {
         this.classLoader = classLoader;
     }
 
+    /**
+     * Get the class loader used to load classes.
+     *
+     * @return a class loader.
+     */
     @NotNull
     public ClassLoader getLoader() {
         return classLoader;
     }
 
+    /**
+     * Get an iterable of classes that are subclasses of the specified type.
+     *
+     * @param type the superclass or interface to filter classes.
+     * @param <T>  the type of the superclass or interface.
+     * @return     an iterable of classes that are subclasses of the specified type.
+     */
     @NotNull
     public <T> Iterable<Class<? extends T>> subClasses(@NotNull Class<T> type) {
         return iterableOf(type::isAssignableFrom, clazz -> clazz.asSubclass(type));
     }
 
+    /**
+     * Get an iterable of classes that are annotated with the specified annotation type.
+     *
+     * @param annotationType the annotation type to filter classes.
+     * @return               an iterable of classes that are annotated with the specified annotation type.
+     */
     @NotNull
     public Iterable<Class<?>> annotated(@NotNull Class<? extends Annotation> annotationType) {
         return iterableOf(clazz -> {
@@ -127,11 +196,25 @@ public class JarRuntime extends LinkedHashMap<String, Class<?>> {
         return false;
     }
 
+    /**
+     * Get an iterable of classes that match the specified predicate.
+     *
+     * @param predicate a predicate to filter classes.
+     * @return          an iterable of classes that match the specified predicate.
+     */
     @NotNull
     public Iterable<Class<?>> iterableOf(@NotNull Predicate<Class<?>> predicate) {
         return iterableOf(predicate, clazz -> clazz);
     }
 
+    /**
+     * Get an iterable of mapped classes that match the specified predicate.
+     *
+     * @param predicate a predicate to filter classes.
+     * @param function  a function to map classes.
+     * @param <T>       the type of the mapped classes.
+     * @return          an iterable of mapped classes that match the specified predicate.
+     */
     @NotNull
     public <T> Iterable<T> iterableOf(@NotNull Predicate<Class<?>> predicate, @NotNull Function<Class<?>, T> function) {
         return new Iterable<T>() {
@@ -170,6 +253,11 @@ public class JarRuntime extends LinkedHashMap<String, Class<?>> {
         };
     }
 
+    /**
+     * Reload all classes that failed to load previously.
+     *
+     * @return this JarRuntime instance.
+     */
     @NotNull
     @Contract("-> this")
     public JarRuntime reload() {
@@ -181,6 +269,13 @@ public class JarRuntime extends LinkedHashMap<String, Class<?>> {
         return this;
     }
 
+    /**
+     * Put a class in the runtime cache.
+     * If the class cannot be loaded, it will be cached as {@code Object.class}.
+     *
+     * @param name the class name to load and put.
+     * @return     the previously cached class, or null if there was no mapping for the class name.
+     */
     @Nullable
     public Class<?> put(@NotNull String name) {
         try {
